e390e5925a9a148d232b011bf0e2c2ad9623be66
G-C0der <31240102+G-C0der@users.noreply.github.com>
2023-11-15 16:22:22 +0100

    env fix

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
-const serverAPIBaseURL = process.env.REACT_APP_SERVER_API_BASE_URL || 'http://localhost:4000/api/v1';
+import Config from "react-native-config";
+
+const serverAPIBaseURL = Config.SERVER_API_BASE_URL || 'http://10.0.2.2:4000/api/v1';
 
 export {
   serverAPIBaseURL
