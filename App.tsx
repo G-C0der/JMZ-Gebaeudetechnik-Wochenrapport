0ed1012430a26dd7e12bfedaf01b094046fc53a1
G-C0der <31240102+G-C0der@users.noreply.github.com>
2023-11-29 08:42:39 +0100

    menu added for workday screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
 import { NavigationContainer } from '@react-navigation/native';
@@ -10,28 +10,18 @@ import { store, StoreContext } from "./src/stores";
 import { navigationRef } from "./src/services";
 import { toastConfig } from "./src/config/toast";
 import './src/config/moment';
-import Button from "./src/components/Button";
+import Menu from "./src/components/Menu";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const hamburger = (
-    <Button
-      icon='menu-fold'
-      iconSize={23}
-      contentColor='#000'
-      bg='transparent'
-      w='25%'
-    />
-  );
-
   return (
     <GluestackUIProvider config={config}>
       <NavigationContainer ref={navigationRef}>
         <StoreContext.Provider value={store}>
             <Stack.Navigator>
-              <Stack.Screen name='Login' component={LoginScreen} />
-              <Stack.Screen name='Arbeitstag' component={WorkdayScreen} options={{ headerRight: () => hamburger }} />
+              {/*<Stack.Screen name='Login' component={LoginScreen} />*/}
+              <Stack.Screen name='Arbeitstag' component={WorkdayScreen} options={{ headerRight: () => <Menu /> }} />
             </Stack.Navigator>
         </StoreContext.Provider>
       </NavigationContainer>
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Menu, MenuItem, Icon, MenuItemLabel, PhoneIcon } from "@gluestack-ui/themed";
+import Button from '../Button';
+
+export default function() {
+  return (
+    <Menu
+      placement="bottom right"
+      offset={0}
+      ml='$20'
+      mt='$8'
+      trigger={({ ...triggerProps }) => {
+        const { 'aria-expanded': isOpen } = triggerProps;
+        return (
+          <Button
+            icon={isOpen ? 'menu-unfold' : 'menu-fold'}
+            iconSize={23}
+            contentColor='#000'
+            bg='transparent'
+            w='25%'
+            {...triggerProps}
+          />
+        );
+      }}
+    >
+      <MenuItem key="Community" textValue="Community">
+        <Icon as={PhoneIcon} size="sm" mr='$2'/>
+        <MenuItemLabel size='sm'>
+          Community
+        </MenuItemLabel>
+      </MenuItem>
+      <MenuItem key="Plugins" textValue="Plugins">
+        {/* PuzzleIcon is imported from 'lucide-react-native' */}
+        <Icon as={PhoneIcon} size="sm" mr='$2'/>
+        <MenuItemLabel size='sm'>
+          Plugins
+        </MenuItemLabel>
+      </MenuItem>
+      <MenuItem key="Theme" textValue="Theme">
+        {/* PaintBucket is imported from 'lucide-react-native' */}
+        <Icon as={PhoneIcon} size="sm" mr='$2'/>
+        <MenuItemLabel size='sm'>
+          Theme
+        </MenuItemLabel>
+      </MenuItem>
+      <MenuItem key="Settings" textValue="Settings">
+        <Icon as={PhoneIcon} size="sm" mr='$2'/>
+        <MenuItemLabel size='sm'>
+          Settings
+        </MenuItemLabel>
+      </MenuItem>
+      <MenuItem key="Add account" textValue="Add account">
+        <Icon as={PhoneIcon} size="sm" mr='$2'/>
+        <MenuItemLabel size='sm'>
+          Add account
+        </MenuItemLabel>
+      </MenuItem>
+    </Menu>
+  );
+}
