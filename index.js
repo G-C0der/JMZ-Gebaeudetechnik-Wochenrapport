01907811a634481d308bf7e091437f9de2e77869
G-C0der <31240102+G-C0der@users.noreply.github.com>
2024-02-02 11:29:32 +0100

    cleanup

--- index.js
+++ index.js
@@ -5,31 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { isDevEnv } from "./src/config/env";
-
-// Hide not resolvable warnings from gluestack beta components for debugging purposes
-if (isDevEnv) {
-  const originalConsoleWarn = console.warn;
-  console.warn = (...args) => {
-    const warningsToIgnore = [
-      /If you do not provide children, you must specify an aria-label for accessibility/i,
-      /<Item> with non-plain text contents is unsupported by type to select for accessibility. Please add a `textValue` prop./i,
-    ];
-    if (warningsToIgnore.some(pattern => pattern.test(args[0]))) {
-      return;
-    }
-    originalConsoleWarn.apply(console, args);
-  };
-  const originalConsoleError = console.error;
-  console.error = (...args) => {
-    const errorsToIgnore = [
-      /Warning: Function components cannot be given refs. Attempts to access this ref will fail. Did you mean to use React.forwardRef()?/i,
-    ];
-    if (errorsToIgnore.some(pattern => pattern.test(args[0]))) {
-      return;
-    }
-    originalConsoleError.apply(console, args);
-  };
-}
 
 AppRegistry.registerComponent(appName, () => App);
